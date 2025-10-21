9e67a3fb627337dd888e2d0b1af334cf28d06a8b
Niharikaaa-1 <2024a6r004@mietjammu.in>
2025-10-22 02:52:42 +0530

    Add greeting program based on user choice
    Implement user choice greeting program

---
+++ Greeting.py
@@ -0,0 +1,13 @@
+'''Write a program to accept choice from user and print result
+1 print Hi
+2 print Welcome
+3 print Bye
+'''
+
+choice = int(input("Enter choice (1, 2, 3): "))
+if choice == 1:
+    print("Hi")
+elif choice == 2:
+    print("Welcome")
+elif choice == 3:
+    print("Bye")
